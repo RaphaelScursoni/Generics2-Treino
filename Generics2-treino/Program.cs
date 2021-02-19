700a98f8ec2629d74320d938e5cbfa628128d55a
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-19 13:59:10 -0300

    Restringindo o uso de Generics para o conteúdo do projeto estudado

---
+++ Generics2-treino/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Generics2_treino.Entities;
+using Generics2_treino.Services;
+
+namespace Generics2_treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculatingService calculationService = new CalculatingService();
+
+            Product p = calculationService.Max(list); // <Product> is optional
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(p);
+        }
+    }
+}
